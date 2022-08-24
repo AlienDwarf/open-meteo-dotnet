fd1d6d6c38a4705b73895fa562cbe77757ff44df
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-08-24 14:41:28 +0200

    Added string WeathercodeToString(int weathercode);

--- OpenMeteo/OpenMeteoClient.cs
+++ OpenMeteo/OpenMeteoClient.cs
@@ -106,6 +106,71 @@ namespace OpenMeteo
             return (response.Cities[0].Latitude, response.Cities[0].Longitude);
         }
 
+        public string WeathercodeToString(int weathercode)
+        {
+            switch (weathercode)
+            {
+                case 0:
+                    return "Clear sky";
+                case 1:
+                    return "Mainly clear";
+                case 2:
+                    return "Partly cloudy";
+                case 3:
+                    return "Overcast";
+                case 45:
+                    return "Fog";
+                case 48:
+                    return "Depositing rime Fog";
+                case 51:
+                    return "Light drizzle";
+                case 53:
+                    return "Moderate drizzle";
+                case 55:
+                    return "Dense drizzle";
+                case 56:
+                    return "Light freezing drizzle";
+                case 57:
+                    return "Dense freezing drizzle";
+                case 61:
+                    return "Slight rain";
+                case 63:
+                    return "Moderate rain";
+                case 65:
+                    return "Heavy rain";
+                case 66:
+                    return "Light freezing rain";
+                case 67:
+                    return "Heavy freezing rain";
+                case 71:
+                    return "Slight snow fall";
+                case 73:
+                    return "Moderate snow fall";
+                case 75:
+                    return "Heavy snow fall";
+                case 77:
+                    return "Snow grains";
+                case 80:
+                    return "Slight rain showers";
+                case 81:
+                    return "Moderate rain showers";
+                case 82:
+                    return "Violent rain showers";
+                case 85:
+                    return "Slight snow showers";
+                case 86:
+                    return "Heavy snow showers";
+                case 95:
+                    return "Thunderstorm";
+                case 96:
+                    return "Thunderstorm with light hail";
+                case 99:
+                    return "Thunderstorm with heavy hail";
+                default:
+                    return "Invalid weathercode";
+            }
+        }
+
         private async Task<WeatherForecast?> GetWeatherForecastAsync(WeatherForecastOptions options)
         {
             try
