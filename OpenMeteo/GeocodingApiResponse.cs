a6e406e9fb289006ce2225d227d1efbb8575c71c
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-08-25 12:01:46 +0200

    CityData renamed to LocationData

--- OpenMeteo/GeocodingApiResponse.cs
+++ OpenMeteo/GeocodingApiResponse.cs
@@ -8,7 +8,7 @@ namespace OpenMeteo
     public class GeocodingApiResponse
     {
         [JsonPropertyName("results")]
-        public CityData[]? Cities { get; set; }
+        public LocationData[]? Cities { get; set; }
         public float Generationtime_ms { get; set; }
 
     }
