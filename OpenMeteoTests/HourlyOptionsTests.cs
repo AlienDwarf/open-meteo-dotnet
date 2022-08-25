7d20e776c420740442db9ade508a695989102ba8
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-08-26 01:34:43 +0200

    test: add more HourlyOptions tests

--- OpenMeteoTests/HourlyOptionsTests.cs
+++ OpenMeteoTests/HourlyOptionsTests.cs
@@ -12,6 +12,18 @@ namespace OpenMeteoTests
     public class HourlyOptionsTests
     {
         [TestMethod]
+        public void HourlyOptions_Add_One_Parameter_Test()
+        {
+            var options = new HourlyOptions();
+
+            Assert.AreEqual(0, options.Parameter.Count);
+            options.Add(HourlyOptionsParameter.winddirection_80m);
+            Assert.AreEqual(1, options.Parameter.Count);
+            Assert.IsTrue(options.Parameter.Contains("winddirection_80m"));
+            Assert.IsFalse(options.Add(HourlyOptionsParameter.winddirection_80m));
+        }
+
+        [TestMethod]
         public void Daily_All_Hourly_All_Test()
         {
             var options = new WeatherForecastOptions(10.5f, 20f);
@@ -29,7 +41,31 @@ namespace OpenMeteoTests
             foreach (string s in HourlyOptions.All.Parameter)
             {
                 Assert.IsTrue(options.Hourly.Parameter.Contains(s));
-            }
+            }   
+        }
+
+        [TestMethod]
+        public void HourlyOptions_Add_Already_Added_Test()
+        {
+            var hourly = HourlyOptions.All;
+
+            Assert.IsFalse(hourly.Add(HourlyOptionsParameter.temperature_2m));
+            Assert.IsFalse(hourly.Add(HourlyOptionsParameter.relativehumidity_2m));
+            Assert.IsFalse(hourly.Add(HourlyOptionsParameter.dewpoint_2m));
+            Assert.IsFalse(hourly.Add(HourlyOptionsParameter.apparent_temperature));
+            Assert.IsFalse(hourly.Add(HourlyOptionsParameter.pressure_msl));
+        }
+
+        [TestMethod]
+        public void HourlyOptions_ArgumentException_Test()
+        {
+            static void action() => new HourlyOptions("A not existing parameter");
+            Assert.ThrowsException<ArgumentException>(action);
+
+            static void action_array() => new HourlyOptions(new string[] { 
+                "A", "not", "existing", "Array", "of", "parameter"
+            });
+            Assert.ThrowsException<ArgumentException>(action_array);
         }
     }
 }
