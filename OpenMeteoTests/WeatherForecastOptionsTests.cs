bef4c7ab8cda8efcd39750b016e2b65cd407a634
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-08-25 22:35:51 +0200

    Test for HourlyOptions

--- OpenMeteoTests/WeatherForecastOptionsTests.cs
+++ OpenMeteoTests/WeatherForecastOptionsTests.cs
@@ -78,25 +78,5 @@ namespace OpenMeteoTests
             Assert.IsTrue(options.Hourly.Parameter.Contains("cloudcover_low"));
             Assert.IsTrue(options.Hourly.Parameter.Contains("cloudcover_high"));
         }
-        [TestMethod]
-        public void Daily_All_Hourly_All_Test()
-        {
-            var options = new WeatherForecastOptions(10.5f, 20f);
-            options.Daily = DailyOptions.All;
-            options.Hourly = HourlyOptions.All;
-
-            Assert.IsTrue(options.Daily.Parameter.Count > 0);
-            Assert.IsTrue(options.Hourly.Parameter.Count > 0);
-            
-            foreach(string s in DailyOptions.All.Parameter)
-            {
-                Assert.IsTrue(options.Daily.Parameter.Contains(s));
-            }
-
-            foreach (string s in HourlyOptions.All.Parameter)
-            {
-                Assert.IsTrue(options.Hourly.Parameter.Contains(s));
-            }
-        }
     }
 }
