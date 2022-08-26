97449f0247d0d0cc72e651ecd467e2b1daa1c397
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-08-26 15:58:36 +0200

    test: add weather forecast test cases
    - Test case for latitude:float longitude:float
    - Test case for custom GeocodingOptions

--- OpenMeteoTests/WeatherForecastTests.cs
+++ OpenMeteoTests/WeatherForecastTests.cs
@@ -11,6 +11,45 @@ namespace OpenMeteoTests
     public class WeatherForecastTests
     {
         [TestMethod]
+        public async Task Only_Location_Name_Test()
+        {
+            OpenMeteoClient client = new OpenMeteoClient();
+            string location = "Tokyo";
+            WeatherForecast weatherData = await client.QueryAsync(location);
+
+            Assert.IsNotNull(weatherData);
+            Assert.IsNotNull(weatherData.Longitude);
+            Assert.IsNotNull(weatherData.Latitude);
+        }
+
+        [TestMethod]
+        public async Task Latitude_Longitude_Test()
+        {
+            OpenMeteoClient client = new OpenMeteoClient();
+            
+            WeatherForecast weatherData = await client.QueryAsync(1f, 2f);
+
+            Assert.IsNotNull(weatherData);
+            Assert.IsNotNull(weatherData.Longitude);
+            Assert.IsNotNull(weatherData.Latitude);
+
+            Assert.AreEqual(1f, weatherData.Latitude);
+            Assert.AreEqual(2f, weatherData.Longitude);
+        }
+
+        [TestMethod]
+        public async Task GeocodingOptions_Test()
+        {
+            OpenMeteoClient client = new OpenMeteoClient();
+            GeocodingOptions options = new GeocodingOptions("Tokyo");
+            WeatherForecast weatherData = await client.QueryAsync(options);
+
+            Assert.IsNotNull(weatherData);
+            Assert.IsNotNull(weatherData.Longitude);
+            Assert.IsNotNull(weatherData.Latitude);
+        }
+
+        [TestMethod]
         public async Task Only_Location_Name_test()
         {
             OpenMeteoClient client = new OpenMeteoClient();
