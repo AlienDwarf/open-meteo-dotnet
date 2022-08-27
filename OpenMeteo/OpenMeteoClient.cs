a9f28a03f38e4e64b166da7a2d1e0b71f2347ae1
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-08-27 18:26:55 +0200

    fix: client uses foreach-loop for options

--- OpenMeteo/OpenMeteoClient.cs
+++ OpenMeteo/OpenMeteoClient.cs
@@ -366,7 +366,7 @@ namespace OpenMeteo
             // Now add the classes Daily & Hourly
 
             // Check that Daily is not empty
-            if (options.Daily.parameter.Count > 0)
+            if (options.Daily.Count > 0)
             {
                 // Add the parameters to query string.
                 // (This CAN'T be the first paramter because latitude and longitude are required and already added)
@@ -449,11 +449,11 @@ namespace OpenMeteo
             }
 
             // Daily
-            if (options.Daily.parameter.Count > 0)
+            if (options.Daily.Count > 0)
             {
                 bool firstDailyElement = true;
                 uri.Query += "&daily=";
-                foreach (string s in options.Daily.parameter)
+                foreach (string s in options.Daily)
                 {
                     if (firstDailyElement)
                     {
