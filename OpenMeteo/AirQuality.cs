441d884ae589110dfe35c2d7547320e36a69e936
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-09-01 23:02:43 +0200

    feat: add air quality api to OpenMeteoClient

--- OpenMeteo/AirQuality.cs
+++ OpenMeteo/AirQuality.cs
@@ -11,7 +11,49 @@ namespace OpenMeteo
         public int UtcOffset { get; set; }
         public string? Timezone { get; set; }
         public string? TimezoneAbbreviation { get; set; }
-        public object? Hourly { get; set; }
-        public object? Hourly_Units { get; set; }
+        public HourlyValues? Hourly { get; set; }
+        public HourlyUnits? Hourly_Units { get; set; }
+
+        public class HourlyUnits
+        {
+            public string? time { get; set; }
+            public string? pm10 { get; set; }
+            public string? pm2_5 { get; set; }
+            public string? carbon_monoxide { get; set; }
+            public string? nitrogen_dioxide { get; set; }
+            public string? sulphur_dioxide { get; set; }
+            public string? ozone { get; set; }
+            public string? aerosol_optical_depth { get; set; }
+            public string? dust { get; set; }
+            public string? uv_index { get; set; }
+            public string? uv_index_clear_sky { get; set; }
+            public string? alder_pollen { get; set; }
+            public string? birch_pollen { get; set; }
+            public string? grass_pollen { get; set; }
+            public string? mugwort_pollen { get; set; }
+            public string? olive_pollen { get; set; }
+            public string? ragweed_pollen { get; set; }
+        }
+
+        public class HourlyValues
+        {
+            public string[]? time { get; set; }
+            public float[]? pm10 { get; set; }
+            public float[]? pm2_5 { get; set; }
+            public float[]? carbon_monoxide { get; set; }
+            public float[]? nitrogen_dioxide { get; set; }
+            public float[]? sulphur_dioxide { get; set; }
+            public float[]? ozone { get; set; }
+            public float[]? aerosol_optical_depth { get; set; }
+            public float[]? dust { get; set; }
+            public float[]? uv_index { get; set; }
+            public float[]? uv_index_clear_sky { get; set; }
+            public float?[]? alder_pollen { get; set; }
+            public float?[]? birch_pollen { get; set; }
+            public float?[]? grass_pollen { get; set; }
+            public float?[]? mugwort_pollen { get; set; }
+            public float?[]? olive_pollen { get; set; }
+            public float?[]? ragweed_pollen { get; set; }
+        }
     }
 }
--- OpenMeteo/AirQualityOptions.cs
+++ OpenMeteo/AirQualityOptions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -7,14 +8,167 @@ namespace OpenMeteo
 {
     public class AirQualityOptions
     {
+        /// <summary>
+        /// Geographical WGS84 coordinate of the location
+        /// </summary>
         public float Latitude { get; set; }
+
+        /// <summary>
+        /// Geographical WGS84 coordinate of the location
+        /// </summary>
         public float Longitude { get; set; }
-        public object Hourly { get; set; }
+
+
+        public HourlyOptions Hourly { get { return _hourly; } set { if (value != null) _hourly = value; } }
         public string Domains { get; set; }
         public string Timeformat { get; set; }
         public string Timezone { get; set; }
         public int Past_Days { get; set; }
         public string Start_date { get; set; }
         public string End_date { get; set; }
+
+        private HourlyOptions _hourly = new HourlyOptions();
+
+        public AirQualityOptions()
+        {
+            Latitude = 0;
+            Longitude = 0;
+            Hourly = new HourlyOptions();
+            Domains = "auto";
+            Timeformat = "iso8601";
+            Timezone = "GMT";
+            Past_Days = 0;
+            Start_date = "";
+            End_date = "";
+        }
+
+        public AirQualityOptions(float latitude, float longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Hourly = new HourlyOptions();
+            Domains = "auto";
+            Timeformat = "iso8601";
+            Timezone = "GMT";
+            Past_Days = 0;
+            Start_date = "";
+            End_date = "";
+        }
+
+        public AirQualityOptions(float latitude, float longitude, HourlyOptions hourly, string domains, string timeformat, string timezone, int past_Days, string start_date, string end_date)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            if (hourly != null)
+                Hourly = hourly;
+            Domains = domains;
+            Timeformat = timeformat;
+            Timezone = timezone;
+            Past_Days = past_Days;
+            Start_date = start_date;
+            End_date = end_date;
+        }
+
+        public class HourlyOptions : IEnumerable<HourlyOptionsParameter>, ICollection<HourlyOptionsParameter>
+        {
+            public static HourlyOptions All { get { return new HourlyOptions((HourlyOptionsParameter[])Enum.GetValues(typeof(HourlyOptionsParameter))); } }
+
+            public List<AirQualityOptions.HourlyOptionsParameter> Parameter { get { return new List<HourlyOptionsParameter>(_parameter); } }
+
+            public int Count => _parameter.Count;
+
+            public bool IsReadOnly => false;
+
+            private readonly List<HourlyOptionsParameter> _parameter = new List<HourlyOptionsParameter>();
+
+            public HourlyOptions(HourlyOptionsParameter parameter)
+            {
+                Add(parameter);
+            }
+
+            public HourlyOptions(HourlyOptionsParameter[] parameter)
+            {
+                Add(parameter);
+            }
+
+            public HourlyOptions()
+            {
+
+            }
+
+            public HourlyOptionsParameter this[int index]
+            {
+                get { return _parameter[index]; }
+                set
+                {
+                    _parameter[index] = value;
+                }
+            }
+
+            public void Add(HourlyOptionsParameter param)
+            {
+                if (this._parameter.Contains(param)) return;
+
+                _parameter.Add(param);
+            }
+
+            public void Add(HourlyOptionsParameter[] param)
+            {
+                foreach (HourlyOptionsParameter paramToAdd in param)
+                {
+                    Add(paramToAdd);
+                }
+            }
+
+            public IEnumerator<HourlyOptionsParameter> GetEnumerator()
+            {
+                return _parameter.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+
+            public void Clear()
+            {
+                _parameter.Clear();
+            }
+
+            public bool Contains(HourlyOptionsParameter item)
+            {
+                return _parameter.Contains(item);
+            }
+
+            public void CopyTo(HourlyOptionsParameter[] array, int arrayIndex)
+            {
+                _parameter.CopyTo(array, arrayIndex);
+            }
+
+            public bool Remove(HourlyOptionsParameter item)
+            {
+                return _parameter.Remove(item);
+            }
+        }
+
+        public enum HourlyOptionsParameter
+        {
+            pm10, 
+            pm2_5, 
+            carbon_monoxide, 
+            nitrogen_dioxide, 
+            sulphur_dioxide, 
+            ozone, 
+            aerosol_optical_depth, 
+            dust, 
+            uv_index, 
+            uv_index_clear_sky, 
+            alder_pollen, 
+            birch_pollen, 
+            grass_pollen, 
+            mugwort_pollen, 
+            olive_pollen, 
+            ragweed_pollen
+        }
     }
 }
