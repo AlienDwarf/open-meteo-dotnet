d721233c759e7cd5b5f6254fa66dd05b94fe950c
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-09-03 01:28:57 +0200

    testing protobuf

---
+++ OpenMeteo/protobuf/Geocoding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+
+namespace OpenMeteo.protobuf
+{
+    [ProtoContract]
+    public class Geocoding
+    {
+        [ProtoMember(1)]
+        public object[]? Locations { get; set; }
+
+        [ProtoMember(2)]
+        public float Generationtime_ms { get; set; }
+    }
+}
