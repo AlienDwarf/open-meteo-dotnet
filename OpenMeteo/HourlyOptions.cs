5cf4ab76fd7eb9d36bf4cf819f1d54224335253b
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-09-11 03:39:02 +0200

    _parameter is initialized in constructor

--- OpenMeteo/HourlyOptions.cs
+++ OpenMeteo/HourlyOptions.cs
@@ -20,62 +20,23 @@ namespace OpenMeteo
 
         public bool IsReadOnly => false;
 
-        [Obsolete]
-        private static readonly string[] _allHourlyParams = new string[]
-        {
-            "temperature_2m",
-            "relativehumidity_2m",
-            "dewpoint_2m",
-            "apparent_temperature",
-            "pressure_msl",
-            "cloudcover",
-            "cloudcover_low",
-            "cloudcover_mid",
-            "cloudcover_high",
-            "windspeed_10m",
-            "windspeed_80m",
-            "windspeed_120m",
-            "windspeed_180m",
-            "winddirection_10m",
-            "winddirection_80m",
-            "winddirection_120m",
-            "winddirection_180m",
-            "windgusts_10m",
-            "shortwave_radiation",
-            "direct_radiation",
-            "diffuse_radiation",
-            "vapor_pressure_deficit",
-            "evapotranspiration",
-            "precipitation",
-            "weathercode",
-            "snow_height",
-            "freezinglevel_height",
-            "soil_temperature_0cm",
-            "soil_temperature_6cm",
-            "soil_temperature_18cm",
-            "soil_temperature_54cm",
-            "soil_moisture_0_1cm",
-            "soil_moisture_1_3cm",
-            "soil_moisture_3_9cm",
-            "soil_moisture_9_27cm",
-            "soil_moisture_27_81cm"
-        };
-
-        private readonly List<HourlyOptionsParameter> _parameter = new List<HourlyOptionsParameter>();
+        private readonly List<HourlyOptionsParameter> _parameter;
 
         public HourlyOptions(HourlyOptionsParameter parameter)
         {
+            _parameter = new List<HourlyOptionsParameter>();
             Add(parameter);
         }
 
         public HourlyOptions(HourlyOptionsParameter[] parameter)
         {
+            _parameter = new List<HourlyOptionsParameter>();
             Add(parameter);
         }
 
         public HourlyOptions()
         {
-
+            _parameter = new List<HourlyOptionsParameter>();
         }
 
         public HourlyOptionsParameter this[int index]
