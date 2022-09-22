5d03f3fabf335d231c006f48055eb2e9eb1f42dc
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2022-09-23 01:28:03 +0200

    fix: typo and documentation for LocationData

--- OpenMeteo/LocationData.cs
+++ OpenMeteo/LocationData.cs
@@ -34,30 +34,86 @@ namespace OpenMeteo
         /// Elevation above sea level in meters.
         /// </summary>
         public float Elevation { get; set; }
+
+        /// <summary>
+        /// Timezone
+        /// </summary>
         public string? Timezone { get; set; }
+
+        /// <summary>
+        /// Type of this location
+        /// </summary>
         [JsonPropertyName("feature_code")]
         public string? FeatureCode { get; set; }
+
+        /// <summary>
+        /// 2-Character country code. "DE" for Germany
+        /// </summary>
         [JsonPropertyName("country_code")]
-        public string? CountyCode { get; set; }
+        public string? CountryCode { get; set; }
+
+        /// <summary>
+        /// Name of the country
+        /// </summary>
         public string? Country { get; set; }
+
+        /// <summary>
+        /// ID for this country
+        /// </summary>
         [JsonPropertyName("country_id")]
         public int CountryId { get; set; }
+
+        /// <summary>
+        /// Population of this country
+        /// </summary>
         public int Population { get; set; }
+
+        /// <summary>
+        /// Postcodes for this location
+        /// </summary>
         public string[]? Postcodes { get; set; }
+
+        /// <summary>
+        /// Name of hierarchical administrative area
+        /// </summary>
         public string? Admin1 { get; set; }
+
+        /// <summary>
+        /// Name of hierarchical administrative area
+        /// </summary>
         public string? Admin2 { get; set; }
+
+        /// <summary>
+        /// Name of hierarchical administrative area
+        /// </summary>
         public string? Admin3 { get; set; }
+
+        /// <summary>
+        /// Name of hierarchical administrative area
+        /// </summary>
         public string? Admin4 { get; set; }
 
+        /// <summary>
+        /// Unique ID for the administrative area
+        /// </summary>
         [JsonPropertyName("admin1_id")]
         public int Admin1Id { get; set; }
 
+        /// <summary>
+        /// Unique ID for the administrative area
+        /// </summary>
         [JsonPropertyName("admin2_id")]
         public int Admin2Id { get; set; }
 
+        /// <summary>
+        /// Unique ID for the administrative area
+        /// </summary>
         [JsonPropertyName("admin3_id")]
         public int Admin3Id { get; set; }
 
+        /// <summary>
+        /// Unique ID for the administrative area
+        /// </summary>
         [JsonPropertyName("admin4_id")]
         public int Admin4Id { get; set; }
     }
